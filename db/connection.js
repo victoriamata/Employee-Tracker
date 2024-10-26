d3aa085607c441527a2155dedffed485262ef063
victoriamata <victoriamataxo@gmail.com>
2024-10-25 22:44:15 -0400

    Added .env config and connected to database

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,20 @@
-// import package
 import pg from 'pg';
-
 const { Pool } = pg;
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const pool = new Pool({
-  user: 'postgres',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   host: 'localhost',
-  database: 'employee_db',
+  database: process.env.DB_NAME,
   port: 5432, // default port//
 });
+// logs messages to advise if the connection was sucessful or failed:
+pool.connect(err => {
+  if (err) throw err;
+  console.log('Database successfully connected.');
+});
 
-
-export default pool;
-
+export default { pool };
